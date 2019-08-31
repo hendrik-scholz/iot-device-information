f2c5cc51efc251f36cf7ab4d63f45ae17f3717c4
hendrik-scholz <scholz-hendrik@web.de>
2019-08-31 17:09:02 +0200

    added request logging

--- src/service/service.ts
+++ src/service/service.ts
@@ -10,6 +10,11 @@ function startService(): void {
     const app: Express = express();
     const port = 3000;
 
+    app.use((req: Request, res: Response, next: any) => {
+        logger.info(`Incoming request: ${req.method}, endpoint: ${req.url}, headers: ${JSON.stringify(req.headers)}.`);
+        next();
+    });
+
     app.get('/authorization', (req: Request, res: Response) => res.send(getAuthorization()));
     app.get('/geoposition', (req: Request, res: Response) => res.send(getGeoposition()));
     app.get('/identification', (req: Request, res: Response) => res.send(getIdentification()));
